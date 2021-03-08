fd7998904dbbc75da8c171e398c10eaab903cee5
umch-dev <chief.umch@gmail.com>
2021-03-08 12:29:16 +0800

    first commit

--- config/winston.js
+++ config/winston.js
@@ -4,8 +4,21 @@
  * Logger
  */
 var winston = require('winston');
-var logger = new (winston.Logger)();
+//var logger = new (winston.Logger)();
 
+const logger = winston.createLogger({
+    level: 'verbose',//process.env.NODE_ENV === 'dev' ? 'debug' : 'info',
+    transports: [new winston.transports.Console()],
+    format: format.combine(
+      format.timestamp(),
+      format.colorize(),
+      format.simple(),
+      format.printf(info => `${info.timestamp} ${info[MESSAGE]}`),
+    ),
+    silent: config.logger.disabled,
+  })
+
+/*  
 logger.add(winston.transports.Console, {
     level: 'verbose',
     prettyPrint: true,
@@ -13,6 +26,7 @@ logger.add(winston.transports.Console, {
     silent: false,
     timestamp: false
 });
+*/
 
 logger.stream = {
     write: function(message, encoding){
